ae0fc90e7e11cea3e073be42f0dff28e1d0149ce
Heng Hakley <Hakleybtb18kh@gmail.com>
2026-04-25 18:47:58 +0700

    fix package with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { componentTagger } from "lovable-tagger";
@@ -13,7 +12,6 @@ export default defineConfig(({ mode }) => ({
     strictPort: true,
   },
   plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
